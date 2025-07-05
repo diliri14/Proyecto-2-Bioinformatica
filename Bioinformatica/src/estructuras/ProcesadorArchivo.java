246008a296da2336b79e8f84fa1d5fcbaa75f5e5
lovera2 <181812169+lovera2@users.noreply.github.com>
2025-07-05 15:04:46 -0400

    Implementaciones - Creacion de la primera version de la interfaz - Creacion de metodos ligados al funciones de la interfaz - Creacion clase ProcesadorArchivo :wq

---
+++ Bioinformatica/src/estructuras/ProcesadorArchivo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuras;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author luismarianolovera
+ */
+public class ProcesadorArchivo {
+
+    public ProcesadorArchivo() {
+    }
+    
+
+   
+       
+    
+}
+
--- Bioinformatica/src/main/Bioinformatica.java
+++ Bioinformatica/src/main/Bioinformatica.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package main;
+import gui.Interfaz1;
 
 /**
  *
@@ -15,6 +16,8 @@ public class Bioinformatica {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Interfaz1 interfaz = new Interfaz1();
+        interfaz.setVisible(true);
     }
     
 }
